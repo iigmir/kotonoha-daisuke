545da86acb826d13f988a493c45fda466b547190
iigmir <roc120j@gmail.com>
2020-04-25 21:20:00 +0800

    Fix cheerio

--- app/parser-module.js
+++ app/parser-module.js
@@ -1,4 +1,3 @@
-const cheerio = require("cheerio");
 const default_text = `<!doctype html><html><head><title>Hello World</title><meta charset="utf-8" /></head><body><div><h1>Hello World</h1></div></body></html>`;
 const Meta = require( "./Meta.js" );
 
