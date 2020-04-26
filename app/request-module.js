8a86dd0674133bcfd71e0e9c9c37274bcb76ffff
iigmir <roc120j@gmail.com>
2020-04-26 12:12:39 +0800

    Add request

--- app/request-module.js
+++ app/request-module.js
@@ -1,9 +1,21 @@
 const axios = require("axios");
 
+function is_in_home_page( input = "" )
+{
+    return input.match( /<title>DQ 地球圖輯隊 帶你看透全世界<\/title>/g );
+}
+
 async function request_module( url = "https://dq.yam.com/post.php?id=12432" ) {
     try {
         const response = await axios.get( url );
-        return response.status === 200 ? response.data : false;
+        if( response.status === 200 && !is_in_home_page )
+        {
+            return response.data;
+        }
+        else
+        {
+            throw new Error( "The article requesting failed!" );
+        }
     } catch (error) {
         throw new Error( error );
     }
