fb4c4ef464a2239143b34c59c6ca205dce0b9d5c
iigmir <roc120j@gmail.com>
2020-05-01 23:24:09 +0800

    I hate async testing

--- spec/basic.spec.js
+++ spec/basic.spec.js
@@ -4,16 +4,16 @@ const assert = require("assert");
 const main = () =>
 {
     describe("request_module", () => {
-        it("should throw get article when the article is in dq.yam.com", () => {
-            assert.doesNotThrow( () => request_module("https://dq.yam.com/post.php?id=12436") );
+        it("should throw get article when the article is in dq.yam.com", async () => {
+            await assert.doesNotThrow( () => request_module("https://dq.yam.com/post.php?id=12436") );
         });
         it("should throw error when the article is invaild in dq.yam.com", async () => {
-            const exp = await request_module( "https://dq.yam.com/post.php?id=Invaild-number" );
-            assert.throws( exp , Error, "The article requesting failed!" );
+            const exp = request_module( "https://dq.yam.com/post.php?id=Invaild-number" );
+            await assert.rejects( exp , Error, "The article requesting failed!" );
         });
         it("should throw error when the article is not in dq.yam.com", async () => {
-            const exp = await request_module( "https://www.example.com" );
-            assert.throws( exp , Error, "The website is not from dq.yam.com!" );
+            const exp = request_module( "https://www.example.com" );
+            await assert.rejects( () => exp , Error, "The website is not from dq.yam.com!" );
         });
     });
 }
