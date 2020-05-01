808f79c80d1abed0e7d53c1c5cddcf39e9c5d07e
iigmir <roc120j@gmail.com>
2020-05-02 00:01:29 +0800

    Delect useless case2 and non-core output file.

--- output.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-module.exports = async (data = {}) =>
-{
-    let json = await data;
-    fs.writeFile( "result.json", JSON.stringify(json), (err) => {
-        if (err) throw err;
-    });
-};
