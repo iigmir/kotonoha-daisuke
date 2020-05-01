51543833bba3c24d0312a51b808d002fc477afed
iigmir <roc120j@gmail.com>
2020-05-01 23:20:15 +0800

    ......??????

--- spec/basic.spec.js
+++ spec/basic.spec.js
@@ -7,8 +7,9 @@ const main = () =>
         it("should throw get article when the article is in dq.yam.com", () => {
             assert.doesNotThrow( () => request_module("https://dq.yam.com/post.php?id=12436") );
         });
-        it("should throw error when the article is invaild in dq.yam.com", () => {
-            assert.throws( () => request_module("https://dq.yam.com/post.php?id=Invaild-number"), /^The article requesting failed!$/ );
+        it("should throw error when the article is invaild in dq.yam.com", async () => {
+            const exp = await request_module( "https://dq.yam.com/post.php?id=Invaild-number" );
+            assert.throws( exp , Error, "The article requesting failed!" );
         });
         it("should throw error when the article is not in dq.yam.com", async () => {
             const exp = await request_module( "https://www.example.com" );
