c847c12b547c0554339fd13250c223d716218907
iigmir <roc120j@gmail.com>
2021-03-09 18:25:09 +0800

    Fix articles

--- app/parser/index.js
+++ app/parser/index.js
@@ -2,23 +2,22 @@ import nhp from "node-html-parser";
 
 export default ( input = "" ) =>
 {
-    const article = nhp.parse( input ).querySelector("article");
-    const reposst_data = article.querySelector( "#mainContent article .repost .box" );
+    const reposst_data = nhp.parse( input ).querySelector( "#mainContent article" );
     return {
         meta: {
-            title: article.querySelector("h1")?.rawText ?? null,
-            date: article.querySelector("time")?.rawText ?? null,
-            author: article.querySelector(".author")?.rawText ?? null,
+            title: reposst_data.querySelector("h1")?.rawText ?? null,
+            date: reposst_data.querySelector("time")?.rawText ?? null,
+            author: reposst_data.querySelector(".author")?.rawText ?? null,
             repost: reposst_data.rawText ?? null,
         },
-        contents: [...article.querySelectorAll(".innerContent p")].map( e=>
+        contents: [...reposst_data.querySelectorAll(".innerContent p")].map( e=>
             e.rawText
         ),
-        images: [...article.querySelectorAll("figure")].map( el => ({
+        images: [...reposst_data.querySelectorAll("figure")].map( el => ({
             src: el.querySelector("img").attributes?.src ?? null,
             txt: el.querySelector("figcaption")?.text ?? null,
         })),
-        references: [...article.querySelectorAll(".sectionWrap a")].map(e=>({
+        references: [...reposst_data.querySelectorAll(".sectionWrap a")].map(e=>({
                 href: e.attributes?.href ?? null,
                 name: e.text ?? null,
             })
