cae9d3db00c06a8f385ed3a98612e31f8105d943
iigmir <roc120j@gmail.com>
2022-03-28 00:37:24 +0800

    write tests

--- spec/basic.spec.js
+++ spec/basic.spec.js
@@ -5,15 +5,11 @@ const main = () =>
 {
     describe("request_module", () => {
         it("should throw get article when the article is in dq.yam.com", async () => {
-            await doesNotThrow( () => request_module("https://dq.yam.com/post.php?id=12436") );
+            await doesNotThrow( () => request_module("12436") );
         });
         it("should throw error when the article is invaild in dq.yam.com", async () => {
-            const exp = request_module( "https://dq.yam.com/post.php?id=Invaild-number" );
+            const exp = request_module( "Invaild-number" );
             await rejects( exp , Error, "The article requesting failed!" );
-        });
-        it("should throw error when the article is not in dq.yam.com", async () => {
-            const exp = request_module( "https://www.example.com" );
-            await rejects( () => exp , Error, "The website is not from dq.yam.com!" );
         });
     });
 }
