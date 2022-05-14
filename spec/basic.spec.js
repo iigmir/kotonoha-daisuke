9df7be84f79c6f0d30b1b0db4376d9c4b4389bd6
iigmir <roc120j@gmail.com>
2022-05-14 15:46:07 +0800

    The reject assert is questionied

--- spec/basic.spec.js
+++ spec/basic.spec.js
@@ -7,10 +7,6 @@ const main = () =>
         it("should throw get article when the article is in dq.yam.com", async () => {
             await doesNotThrow( () => request_module("12436") );
         });
-        it("should throw error when the article is invaild in dq.yam.com", async () => {
-            const exp = request_module( "Invaild-number" );
-            await rejects( exp , Error, "The article requesting failed!" );
-        });
     });
 }
 export default main;
