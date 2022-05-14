a71187536280acad2778e26dadb5053dbea49485
iigmir <roc120j@gmail.com>
2022-05-14 14:16:59 +0800

    Sperate ajax library

--- app/ajax/index.js
+++ app/ajax/index.js
@@ -1,24 +1,7 @@
-import https from "https";
+import library from "./library.js";
 
 export default (input = "12436") =>
 {
     const url = `https://dq.yam.com/post/${String(input)}`;
-    return new Promise( (resolve, reject)  => {
-        const req = https.get(url, res =>
-        {
-            res.setEncoding("utf8");
-            let html_text = "";
-            res.on( "data", chunk => { html_text += chunk });
-            req.on( "error", error => { reject(error) });
-            res.on( "end", () =>
-            {
-                try {
-                    resolve(html_text);
-                } catch (e) {
-                    console.error(e.message);
-                    reject(e);
-                }
-            });
-        });
-    });
+    return new Promise( library(url) );
 };
--- app/ajax/library.js
+++ app/ajax/library.js
@@ -1,20 +1,21 @@
 import https from "https";
 
-function ajax(url) {
-    const req = https.get(url, res => {
-        res.setEncoding("utf8");
-        let html_text = "";
-        res.on("data", chunk => { html_text += chunk; });
-        req.on("error", error => { reject(error); });
-        res.on("end", () => {
-            try {
-                resolve(html_text);
-            } catch (e) {
-                console.error(e.message);
-                reject(e);
-            }
+export default (url) => {
+    return (resolve, reject) => {
+        const req = https.get(url, res => {
+            res.setEncoding("utf8");
+            let html_text = "";
+            res.on("data", chunk => { html_text += chunk; });
+            req.on("error", error => { reject(error); });
+            res.on("end", () => {
+                try {
+                    resolve(html_text);
+                } catch (e) {
+                    console.error(e.message);
+                    reject(e);
+                }
+            });
         });
-    });
-}
+    };
+};
 
-export default ajax;
